543fa7b4e0556aa30484f6b008a8a74e34be79c6
o.kovalchuk <mail.to.kovalchuk@gmail.com>
2022-03-09 13:24:31 +0200

    Pg connect

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,6 @@
 module.exports = [
   "strapi::errors",
   "strapi::security",
-  // {
-  //   name: "strapi::cors",
-  //   config: {
-  //     enabled: true,
-  //     header: "*",
-  //     origin: ["http://localhost:1337", "http://localhost:3000"],
-  //   },
-  // },
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
